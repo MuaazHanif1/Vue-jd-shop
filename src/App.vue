de1c225bec78f705210cb76d6ef9ea1c570b6f16
MuaazHanif1 <muaazhanif2@gmail.com>
2026-02-10 15:39:44 +0500

    'first'

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+<HeaderComponent/>
+<increment title="MY Website 1" subtitle="this is my website" :list="[]" @childToParent="Parent" @childtoparent2="Parent">
+</increment>
+<h1>{{childValues?.name}}</h1>
+<br>
+
+</template>
+<script setup>
+import {ref} from 'vue'
+import increment from './components/increment.vue'
+import HeaderComponent from './components/Header-Component.vue';
+const listOfStudent = ['Hamza',"Talha"]
+let childValues = ref({})
+childValues.value = [];
+
+function Parent(data){
+  //childValues.value = data
+  consoles.log(data)
+}
+</script>
+
+<style>
+/* .color-blue{
+  color:blue
+} */
+</style>
---
+++ src/components/Header-Component.vue
@@ -0,0 +1,14 @@
+<template>
+    <header class="flex header">
+        <div class="logo">
+           <h1 class="logo-font-style">JD</h1>
+        </div>
+        <nav class="nav">
+           <a href="/index.html" class="margin-10 nav-link">Home</a>
+           <a href="#" class="margin-10 nav-link">Shop</a>
+           <a href="./about.html" class="margin-10 nav-link">About</a>
+           <a href="#" class="margin-10 nav-link">Contact</a>
+           <a href="./addnewproduct.html">New Product</a>
+        </nav>
+    </header>
+</template>
