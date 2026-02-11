75fdc62b9d81f98e88da66fd7b2a64d07c9a3f79
M.ABDULLAH ARSHAD <ra6284502>
2026-02-11 20:18:40 +0500

    Abdullah

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,47 @@
 <template>
 <HeaderComponent/>
-<h1 style="color: aliceblue; background-color: aqua;">M.ABDULLAH ARSHAD</h1>
-<h1>Talha</h1>
-<h1>Welcome to my website 1</h1>
-<br>
-<h1>Maria!!</h1>
-<h1>iqra</h1>
-<h1>umaima</h1>
-<h1>Naqi sab</h1>
+<div class="container">
+<div class="students">
+  <h1>Students in Web development 2nd batch</h1>
+  <ol>
+<li>M.ABDULLAH ARSHAD</li>
+<li>Talha</li>
+<li>Maria!!</li>
+<li>iqra</li>
+<li>umaima</li>
+<li>Naqi sab</li>
+</ol>
+</div>
+</div>
 </template>
 <script setup>
 import HeaderComponent from './components/Header-Component.vue';
 </script>
 
-<style>
+<style scoped>
+.container{
+  display: flex;
+  justify-content: center;
+  margin-top: 30px;
+  height: 50vh;
+}
+.students{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border: 2px solid black;
+  border-radius: 20px;
+  padding: 20px;
+}
+li{
+  font-size: 23px;
+  margin-top: 10px;
+  text-transform: uppercase;
+}
+h1{
+  color:blue;
+  text-transform: capitalize;
+}
 /* .color-blue{
   color:blue
 } */
