5bea1384bb11af47d1f21b039935c34fed7b5cac
M.ABDULLAH ARSHAD <ra6284502>
2026-03-03 20:39:47 +0500

    ISomeChange

--- src/components/Product-List-Component.vue
+++ src/components/Product-List-Component.vue
@@ -1,9 +1,6 @@
 <template>
     <section class="products mt-[60px] mb-[60px] mr-[40px] ml-[40px]">
-         <h2>Featured Products</h2>
-         <h1 class="text-3xl font-bold text-underline">
-            Hello world!
-          </h1>
+         <h2 class="bg-linear-to-r from-pink-500 to-violet-500 bg-clip-text text-5xl font-extrabold text-transparent uppercase underline margin-top-10">Featured Products</h2>
          <div id="product-list" class="product-list flex-center">
          <CardComponent v-for="value in productData" 
                     :productId="value.id"
