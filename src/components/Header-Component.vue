5a92f58481a3f47ef0646124ecce60944ffac838
MuaazHanif1 <muaazhanif2@gmail.com>
2026-03-03 15:38:18 +0500

    'tailwind'

--- src/components/Header-Component.vue
+++ src/components/Header-Component.vue
@@ -1,7 +1,7 @@
 <template>
     <header class="flex header">
         <div class="logo">
-           <h1 class="logo-font-style">JD</h1>
+           <h1 class="logo-font-style text-[30px]">JD</h1>
         </div>
         <nav class="nav">
            <!-- <a href="/index.html" class="margin-10 nav-link">Home</a>
