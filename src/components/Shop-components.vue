5c9a5dc5b8079f289732bafd459e66eb4a11b8bc
M.ABDULLAH ARSHAD <ra6284502>
2026-03-04 21:50:02 +0500

    ShopPageCreate

--- src/components/Shop-components.vue
+++ src/components/Shop-components.vue
@@ -1,17 +1,53 @@
 <template>
-  <div class="p-[30px]">
-
-    <div 
-      v-for="item in store.myArray" 
-      :key="item.id"
-      class="product-card shadow-xl/20 rounded-lg p-4 mb-6"
-    >
-      <img :src="item.image" :alt="item.title" class="w-[200px]">
-      <h3 class="font-bold mt-2">{{ item.title }}</h3>
-      <p>Rs: {{ item.price }}</p>
+  <div style="display: flex; justify-content: center; align-items: center; ">
+  <div style="display: flex; justify-content: center; align-items: center; background-color: white; border: 2px solid black; width: 70%; padding: 20px; border-radius: 10px; margin-top: 30px; margin-bottom: 20px;">
+     <div v-if="store.myArray.length === 0" class="text-2xl font-bold text-gray-500">
+      Your cart is empty.
     </div>
-
+    <div 
+      v-for="item in store.myArray"
+      :key="item.id"
+      style="display: flex; justify-content: space-between; align-items: center; border: 1px solid #ccc; padding: 20px; border-radius: 10px; margin-bottom: 20px; width: 100%;"
+      class="bg-white"
+    >
+    <!-- LEFT SIDE (Image + Details) -->
+      <div class="flex gap-6 items-center">
+        <img 
+          :src="item.image"
+          class="w-[140px] h-[140px] object-contain"
+        />
+        <div>
+          <h2 class="text-lg font-semibold w-[500px]">
+            {{ item.title }}
+          </h2>
+          <!-- Quantity Controls -->
+          <div class="flex items-center gap-3 mt-4">
+            <!-- Delete Icon -->
+            <button 
+              @click="store.removeItem(item.id)"
+              class="text-gray-600 hover:text-red-600 "
+            >🗑</button>
+            <!-- Quantity Box -->
+            <div class="flex items-center border rounded-full overflow-hidden">
+              <button 
+                @click="store.decreaseQty(item.id)" style="background-color: none; color: black;">-</button>
+              <span class="px-4 text-4xl font-semibold" style="padding-left: 14px; padding-right: 14px;">{{ item.quantity }}</span>
+              <button 
+                @click="store.increaseQty(item.id)" style="background-color: none; color: black;">+</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- RIGHT SIDE (Price) -->
+      <div class="text-xl font-bold text-green-700">
+        Rs {{ item.price * item.quantity }}
+      </div>
+    </div>
   </div>
+  </div>
+  <div>
+    <h2 class="bg-linear-to-r from-pink-500 to-violet-500 bg-clip-text text-5xl font-extrabold text-transparent uppercase underline mt-60px text-center mb-4" style="margin-bottom: 20px;">Total Price: {{ store.totalPrice }}</h2>
+    </div>
 </template>
 
 <script setup>
--- src/store/MyStore.js
+++ src/store/MyStore.js
@@ -1,12 +1,51 @@
 import { defineStore } from 'pinia'
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 
 export const useMyStore = defineStore('myStore', () => {
-  const myArray = ref([])  // Ye tumhara array hai
+
+  const myArray = ref([])
 
   const addItem = (item) => {
-    myArray.value.push(item)
+    const existingItem = myArray.value.find(p => p.id === item.id)
+
+    if (existingItem) {
+      existingItem.quantity++
+    } else {
+      myArray.value.push({
+        ...item,
+        quantity: 1   // default 1
+      })
+    }
   }
 
-  return { myArray, addItem }
+  const increaseQty = (id) => {
+    const item = myArray.value.find(p => p.id === id)
+    if (item) item.quantity++
+  }
+
+  const decreaseQty = (id) => {
+    const item = myArray.value.find(p => p.id === id)
+    if (item && item.quantity > 1) {
+      item.quantity--
+    }
+  }
+
+  const removeItem = (id) => {
+    myArray.value = myArray.value.filter(p => p.id !== id)
+  }
+
+  const totalPrice = computed(() => {
+    return myArray.value.reduce((total, item) => {
+      return total + (Number(item.price) * item.quantity)
+    }, 0)
+  })
+
+  return {
+    myArray,
+    addItem,
+    increaseQty,
+    decreaseQty,
+    removeItem,
+    totalPrice
+  }
 })
